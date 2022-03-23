1e44b1208ea59b795a9f345dfdc212f2bbf850f8
belayneh getachew <87185500+elayneh@users.noreply.github.com>
2022-03-23 15:08:16 -0400

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,16 +4,15 @@
 apt-get update
 apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
-mkdie -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo '<html>
   <head>
   </head>
   <body>
-    This is the content for the html file to test if it is created
+    Holberton School
   </body>
 </html>' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/release/test/ /data/web_static_current
-chown -hR ubuntu:ubuntu /data
-sed -i '51 i \\n\tlocation /hbnt_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
