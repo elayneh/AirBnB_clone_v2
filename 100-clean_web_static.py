a4f52bdd648d159b848117b7ca9105c55ec13d48
elayneh <betenatefera@gmail.com>
2022-03-24 13:53:20 -0400

    modified

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,21 +2,23 @@
 # clean out of dated archive from the deployment
 from fabric.api import *
 import os
-from datetime import datetime
+
 env.hosts = ['34.139.204.59', '3.235.148.75']
 
 
 def do_clean(number=0):
-    """remove out-datedarchives"""
+    """remove out-dated archives
+    """
 
     number = 1 if int(number) == 0 else int(number)
 
-    archs = sorted(os.listdir("version"))
-    [archs.pop() for elt in range(number)]
-    with lcd("version"):
-        [local("rm ./{}".format(item)) for item in archs]
-    with cd("/data/web_static_releases"):
+    archs = sorted(os.listdir("versions"))
+    [archs.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archs]
+
+    with cd("/data/web_static/releases"):
         archs = run("ls -tr").split()
-        archs = [item for item in archs if "web_static_" in item]
-        [archs.pop() for item in range(number)]
-        [run("rm -rf ./{}".format(item)) for item in archs]
+        archs = [a for a in archs if "web_static_" in a]
+        [archs.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archs]
