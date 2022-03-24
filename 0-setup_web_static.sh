7f07c336e5d14f5714c658a7a9c4aee4fdee0d62
elayneh <betenatefera@gmail.com>
2022-03-24 11:27:46 -0400

    shellchecked

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # server setup to deployment
 
-apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-sudo echo "<html>
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo '<html>
   <head>
   </head>
   <body>
-    ALX School
+    Holberton School
   </body>
-</html>" > | sudo tee /data/web_static/releases/test/index.html
+</html>' > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
