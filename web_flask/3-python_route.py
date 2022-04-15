732dfefb002af0e4532408ab50a3f26c60a481ba
elayneh <betenatefera@gmail.com>
2022-04-15 11:31:08 -0400

    modified

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,56 +1,39 @@
 #!/usr/bin/python3
+""" 3. Add third view func that redirects and has default val for variable """
 
 from flask import Flask
+
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def HHB():
-    """_summary_
-
-    Returns:
-        _type_: _description_
-    """
-    return "Hello HBNB!"
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
-def hbnh():
-    """_summary_
-
-    Returns:
-        _type_: _description_
-    """
-    return "HBNB"
+def hello():
+    """ Return other text. """
+    return 'HBNB'
 
 
 @app.route('/c/<text>')
-def disp(text):
-    """_summary_
-
-    Args:
-        text (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    return "C {}".format(text.replace("_", " "))
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-@app.route('/python')
+@app.route('/python/')
 @app.route('/python/<text>')
-def dispPy(text='is cool'):
-    """_summary_
-
-    Args:
-        text (str, optional): _description_. Defaults to 'is cool'.
-
-    Returns:
-        _type_: _description_
-    """
-    return "Python {}".format(text.replace("_", " "))
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
-if (__name__ == "__main__"):
-    app.run('host=0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
