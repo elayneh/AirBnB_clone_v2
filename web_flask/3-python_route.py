bd74675561cbe7e3ff5f6aff2407bf68f4539bf3
elayneh <betenatefera@gmail.com>
2022-04-15 11:29:23 -0400

    modified

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,38 +1,56 @@
 #!/usr/bin/python3
 
 from flask import Flask
-
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
+def HHB():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb')
-def hello():
-    """ Return other text. """
-    return 'HBNB'
+def hbnh():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return "HBNB"
 
 
 @app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+def disp(text):
+    """_summary_
+
+    Args:
+        text (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/')
+@app.route('/python')
 @app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+def dispPy(text='is cool'):
+    """_summary_
+
+    Args:
+        text (str, optional): _description_. Defaults to 'is cool'.
+
+    Returns:
+        _type_: _description_
+    """
+    return "Python {}".format(text.replace("_", " "))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if (__name__ == "__main__"):
+    app.run('host=0.0.0.0', port=5000)
