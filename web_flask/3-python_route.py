47091e800b044ba5e0685d008c66aed19d5a74dd
elayneh <betenatefera@gmail.com>
2022-04-15 11:26:25 -0400

    modified

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,28 +1,54 @@
 #!/usr/bin/python3
 
-
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def HHB():
-    return "Hello HBNB"
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnh():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def disp(text):
+    """_summary_
+
+    Args:
+        text (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
 def dispPy(text='is cool'):
+    """_summary_
+
+    Args:
+        text (str, optional): _description_. Defaults to 'is cool'.
+
+    Returns:
+        _type_: _description_
+    """
     return "Python {}".format(text.replace("_", " "))
 
 
